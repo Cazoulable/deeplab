a4eef28a6693bc29e51b415aebc7314e6f6d419e
Aaditya Prakash <aaadityaprakash@gmail.com>
2019-08-07 00:15:56 -0400

    export models for inference

--- deeplab/export_model.py
+++ deeplab/export_model.py
@@ -32,10 +32,10 @@ flags.DEFINE_string('checkpoint_path', None, 'Checkpoint path')
 flags.DEFINE_string('export_path', None,
                     'Path to output Tensorflow frozen graph.')
 
-flags.DEFINE_integer('num_classes', 21, 'Number of classes.')
+flags.DEFINE_integer('num_classes', 4, 'Number of classes.')
 
-flags.DEFINE_multi_integer('crop_size', [513, 513],
-                           'Crop size [height, width].')
+flags.DEFINE_list('crop_size', '513,513',
+                  'Image crop size [height, width]')
 
 # For `xception_65`, use atrous_rates = [12, 24, 36] if output_stride = 8, or
 # rates = [6, 12, 18] if output_stride = 16. For `mobilenet_v2`, use None. Note
@@ -91,8 +91,8 @@ def _create_input_tensors():
     resized_image, image, _ = input_preprocess.preprocess_image_and_label(
         image,
         label=None,
-        crop_height=FLAGS.crop_size[0],
-        crop_width=FLAGS.crop_size[1],
+        crop_height=int(FLAGS.crop_size[0]),
+        crop_width=int(FLAGS.crop_size[1]),
         min_resize_value=FLAGS.min_resize_value,
         max_resize_value=FLAGS.max_resize_value,
         resize_factor=FLAGS.resize_factor,
@@ -116,7 +116,7 @@ def main(unused_argv):
 
         model_options = common.ModelOptions(
             outputs_to_num_classes={common.OUTPUT_TYPE: FLAGS.num_classes},
-            crop_size=FLAGS.crop_size,
+            crop_size=[int(i) for i in FLAGS.crop_size],
             atrous_rates=FLAGS.atrous_rates,
             output_stride=FLAGS.output_stride)
 
--- test.sh
+++ test.sh
@@ -1,8 +1,6 @@
 VERSION_ID=0
-MODEL=deeplabv3_pascal_trainval
 
 PROJECT_DIR=/home/ap/steel
-PATH_TO_INITIAL_CHECKPOINT=$PROJECT_DIR/models/$MODEL/model.ckpt
 PATH_TO_TRAIN_DIR=$PROJECT_DIR/experiments/v$VERSION_ID/model.ckpt-47689
 PATH_TO_VAL_DIR=$PROJECT_DIR/experiments/evals/
 PATH_TO_DATASET=$PROJECT_DIR/data/tfrecord/
